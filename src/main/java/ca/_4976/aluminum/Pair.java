567bf4e74374e93443ebf8ae1d868cd1c2b487a8
Marc Levesque <Qormix@gmail.com>
2017-06-04 02:30:13 -0400

    Added New Features

---
+++ src/main/java/ca/_4976/aluminum/Pair.java
@@ -0,0 +1,7 @@
+package ca._4976.aluminum;
+
+/**
+ * Created by Qormix on 2017-06-04.
+ */
+public class Pair {
+}
---
+++ src/main/java/ca/_4976/aluminum/RampedMotor.java
@@ -0,0 +1,7 @@
+package ca._4976.aluminum;
+
+/**
+ * Created by Qormix on 2017-06-03.
+ */
+public class RampedMotor {
+}
